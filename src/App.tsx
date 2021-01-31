5f55d9d40330e5ed616981987e0a5c2ac250c502
ryochansq <nicestsnsq@gmail.com>
2021-02-01 00:11:15 +0900

    add header & test state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core'
+
+type Mode = 'Top' | 'Game' | 'Result'
 
 const App: React.FC = () => {
+  const [mode, setMode] = useState<Mode>('Top')
+
+  const onClickStart = () => {
+    const setModeFunc = (currentMode: Mode) => {
+      if (currentMode === 'Top') return 'Game'
+      else if (currentMode === 'Game') return 'Result'
+      else return 'Top'
+    }
+    setMode(setModeFunc)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>ぎみちょこ</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            ギミチョコ
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {mode === 'Top' && <p>とっぷ</p>}
+      {mode === 'Game' && <p>げーむ</p>}
+      {mode === 'Result' && <p>りざると</p>}
+      <Button variant="contained" color="primary" onClick={onClickStart}>
+        スタート！
+      </Button>
     </div>
   )
 }
